4959506c22e1eb6a64d08d7ffd8d1e0a8ca2fe2a
Athos Innocenti <57680842+athos-innocenti@users.noreply.github.com>
2021-11-17 11:53:21 +0100

    Update error_per_angle.m

--- error_per_angle.m
+++ error_per_angle.m
@@ -1,11 +1,13 @@
 function [avgError, avgTime] = error_per_angle(originalModel, transformedModel, triesPerAngle, angleStep, maxTraslation, maxIterations)
-    avgError = zeros(360/angleStep + 1);
-    avgTime = zeros(360/angleStep + 1);
+    avgError = zeros(1, 360/angleStep + 1);
+    avgTime = zeros(1, 360/angleStep + 1);
     vectorIndex = 1;
     for angleIter = 0:angleStep:360
-        errors = zeros(triesPerAngle);
-        times = zeros(triesPerAngle);
+        errors = zeros(1, triesPerAngle);
+        times = zeros(1, triesPerAngle);
         for t = 1:triesPerAngle
+            fprintf('Angle: %d Iter: %d \n', angleIter, t);
+
             originalCloud = pcread(originalModel);
             transformedCloud = pcread(transformedModel);
             
@@ -18,12 +20,14 @@ function [avgError, avgTime] = error_per_angle(originalModel, transformedModel, 
             % [~, movingReg, rmse] = pcregistericp(ptCloudTransformed, originalCloud, 'Metric', 'pointToPoint', 'MaxIterations', maxIterations);
             % pcshowpair(originalCloud, movingReg);
             elapsedTime = toc;
+            elapsedTime = elapsedTime * 1000;
             
-            errors(t) = rmse;
-            times(t) = elapsedTime;
+            fprintf('Error: %f Time[ms]: %f \n\n', rmse, elapsedTime);
+            errors(1, t) = rmse;
+            times(1, t) = elapsedTime;
         end
-        avgError(vectorIndex) = mean(errors);
-        avgTime(vectorIndex) = mean(times);
+        avgError(1, vectorIndex) = mean(errors);
+        avgTime(1, vectorIndex) = mean(times);
         vectorIndex = vectorIndex + 1;
     end
 end
