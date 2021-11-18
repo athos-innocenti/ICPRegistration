7114f5a95dacefef8493fc4115b1fbc8919cdd23
Athos Innocenti <57680842+athos-innocenti@users.noreply.github.com>
2021-11-18 15:01:52 +0100

    Update icp.m

--- icp.m
+++ icp.m
@@ -3,8 +3,8 @@ clc
 close all
 
 % Initial rotation's angle from 0 to 2PI with angle's step of PI/6
-ANGLE_STEP = 30;
-% Number of tries for each initial combination: 1D/3D rotation+ random 3D traslation
+ANGLES = 0:30:360;
+% Number of tries for each initial combination: 1D/3D rotation + random 3D traslation
 TRIES_ANGLE = 100;
 % Number of tries for each initial combination: random 3D rotation + random 3D traslation
 TRIES_ITER = 100;
@@ -14,26 +14,32 @@ MAX_ITERATIONS = 100;
 MAX_TRASLATION = 1.0;
 % Random initial 3D rotation matrix, different for each try, with max rotation = 360°
 MAX_ROTATION = 360;
-% Original point cloud (ends with 0)
+% Original point cloud name (ends with 0)
 ORIGINAL_MODEL = './model/office_chair_0.ply';
-% Transformed point cloud (ends with 1 - moving point cloud with fewer points)
+% Transformed point cloud name (ends with 1 - moving point cloud with fewer points)
 TRANSFORMED_MODEL = './model/office_chair_1.ply';
+% model identifier for .png and .csv files creation
+MODEL_ID = 'oc';
 
 % Given an initial rotation's angle Theta from 0° to 360° with step of 30°,
 % run ICP algorithm for TRIES_ANGLE times with different 3D random initial translation vector for each try.
 % ICP max iterations is always the same.
-% Possible initial rotations analysed:
-%   1. (ThetaX, 0, 0)
-%   2. (0, ThetaY, 0)
-%   3. (0, 0, ThetaZ)
-%   4. (Theta, Theta, Theta) same rotation angle w.r.t X Y and Z axes
-[avgError, avgTime] = error_per_angle(ORIGINAL_MODEL, TRANSFORMED_MODEL, TRIES_ANGLE, ANGLE_STEP, MAX_TRASLATION, MAX_ITERATIONS);
+% Initial rotations analysed (same angle for X-Y-Z axes in 3D rotation):
+%   1. (Theta, 0, 0)            -> rotType = 'X'
+%   2. (0, Theta, 0)            -> rotType = 'Y'
+%   3. (0, 0, Theta)            -> rotType = 'Z'
+%   4. (Theta, Theta, Theta)    -> rotType = '3D'
+rotType = '3D';
+[avgError, avgTime] = error_per_angle(ORIGINAL_MODEL, TRANSFORMED_MODEL, TRIES_ANGLE, ANGLES, MAX_TRASLATION, MAX_ITERATIONS, rotType);
 
 % For TRIES_ITER times, randomly define an initial 3D rotation + translation matrix
 % and then apply ICP algorithm 3 times: the first time with 50 max iterations,
 % the second with 100 max iterations and the last one with 200
-[errors, times] = error_per_iter(ORIGINAL_MODEL, TRANSFORMED_MODEL, TRIES_ITER, MAX_TRASLATION, MAX_ROTATION, [50, 100, 200]);
+% [errors, times] = error_per_iter(ORIGINAL_MODEL, TRANSFORMED_MODEL, TRIES_ITER, MAX_TRASLATION, MAX_ROTATION, [50, 100, 200]);
 
-angles = 0:30:360;
-plot_values(angles, avgError, 'Error');
-plot_values(angles, avgTime, 'Time');
+% Plot average errors and times, save plots as .png files and save values
+% in .csv files. Example for file name: oc_error3D_100i.png
+plot_values(ANGLES, avgError, 'Error', MODEL_ID, rotType, MAX_ITERATIONS);
+plot_values(ANGLES, avgTime, 'Time', MODEL_ID, rotType, MAX_ITERATIONS);
+
+% TO DO: grafico a torta di errors
