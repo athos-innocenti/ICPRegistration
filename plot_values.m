9865ed234e701d3989430c56b9c3583926c1102e
Athos Innocenti <57680842+athos-innocenti@users.noreply.github.com>
2021-11-25 13:46:27 +0100

    x limit added

--- plot_values.m
+++ plot_values.m
@@ -2,6 +2,7 @@ function [] = plot_values(x, y, title, modelId, rotType, numIters)
     plot(x, y, 'b--o');
     xlabel('Angle');
     ylabel(title);
+    xlim([0 360]);
     plt = gca;
     plotFileName = ['./performance/', modelId, '_', lower(title), rotType, '_', num2str(numIters), 'i.png'];
     exportgraphics(plt, plotFileName, 'Resolution', 300);
