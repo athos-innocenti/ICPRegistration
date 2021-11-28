fa5e2ecf3ca7bdb86ab6aab8643a26102ab330ac
Athos Innocenti <57680842+athos-innocenti@users.noreply.github.com>
2021-11-28 17:34:24 +0100

    Resolution fixed

--- plot_values.m
+++ plot_values.m
@@ -5,7 +5,7 @@ function [] = plot_values(x, y, title, modelId, rotType, numIters, maxAngle)
     xlim([0 maxAngle]);
     plt = gca;
     plotFileName = ['./performance/', modelId, '_', lower(title), rotType, '_', num2str(numIters), 'i.png'];
-    exportgraphics(plt, plotFileName, 'Resolution', 300);
+    exportgraphics(plt, plotFileName, 'Resolution', 600);
     dataFileName = ['./performance/', modelId, '_', lower(title), rotType, '_', num2str(numIters), 'i.csv'];
     writematrix(transpose(y), dataFileName);
 end
