6168db1a9ae82f433ff87b0606409a58f93e3bc2
Athos Innocenti <57680842+athos-innocenti@users.noreply.github.com>
2021-12-07 17:05:03 +0100

    Knn for initial rmse and pcshowpair added

--- error_per_iter.m
+++ error_per_iter.m
@@ -1,42 +1,42 @@
-function [initialErrors, errors, times] = error_per_iter(originalModel, transformedModel, triesPerIter, maxTraslation, maxRotation, maxIterationsVector)
+function [initialErrors, errors, times] = error_per_iter(originalModel, transformedModel, triesPerIter, maxTranslation, maxRotation, maxIterationsVector)
     initialErrors = zeros(triesPerIter, length(maxIterationsVector));
     errors = zeros(triesPerIter, length(maxIterationsVector));
     times = zeros(triesPerIter, length(maxIterationsVector));
     for t = 1:triesPerIter
-        % Generate 1 integer from the uniform distribution between 
+        % Define a random 3D rotation from the uniform distribution between 
         % 0 and maxRotation
         rotX = randi([0 maxRotation], 1, 1);
         rotY = randi([0 maxRotation], 1, 1);
         rotZ = randi([0 maxRotation], 1, 1);
         
-        % Generate a 1-by-1 vector of uniformly distributed numbers in 
-        % the interval (0, maxTraslation)
-        trsX = 0 + (maxTraslation - 0)*rand(1, 1);
-        trsY = 0 + (maxTraslation - 0)*rand(1, 1);
-        trsZ = 0 + (maxTraslation - 0)*rand(1, 1);
+        % Define a random 3D translation vector where each component is
+        % uniformly distributed in the interval [0, maxTranslation]
+        trsX = 0 + (maxTranslation - 0)*rand(1, 1);
+        trsY = 0 + (maxTranslation - 0)*rand(1, 1);
+        trsZ = 0 + (maxTranslation - 0)*rand(1, 1);
         
         columnIndex = 1;
         for maxIter = maxIterationsVector
             fprintf('Try:%d MaxIteration:%d \n', t, maxIter);
             fprintf('ROTATION x:%d° y:%d° z:%d° \n', rotX, rotY, rotZ);
-            fprintf('TRASLATION x:%3f y:%3f z:%3f \n', trsX, trsY, trsZ);
+            fprintf('TRANSLATION x:%3f y:%3f z:%3f \n', trsX, trsY, trsZ);
             
             originalCloud = pcread(originalModel);
-            transformedCloud = pcread(transformedModel);
+            movingCloud = pcread(transformedModel);
             
             initialTransformation = transformation_matrix(deg2rad(rotX), deg2rad(rotY), deg2rad(rotZ), trsX, trsY, trsZ);
-            ptCloudTransformed = pctransform(transformedCloud, initialTransformation);
+            transformedCloud = pctransform(movingCloud, initialTransformation);
             
             % Lines 32-33 define the initial rmse between originalCloud and
             % ptCloudTransformed  before ICP is applied
-            [~, pointDistances] = knnsearch(originalCloud.Location, ptCloudTransformed.Location);
+            [~, pointDistances] = knnsearch(originalCloud.Location, transformedCloud.Location);
             initialRmse = sqrt(sum(pointDistances) / numel(pointDistances));
             initialErrors(t, columnIndex) = initialRmse;
             
             % For plotting resulting ICP cloud and original cloud uncomment
             % lines 43-45 and change [~, ~, rmse] with [~, movingReg, rmse]
             tic;
-            [~, ~, rmse] = pcregistericp(ptCloudTransformed, originalCloud, 'Metric', 'pointToPoint', 'MaxIterations', maxIter, 'Tolerance', [0.001, 0.001]);
+            [~, ~, rmse] = pcregistericp(transformedCloud, originalCloud, 'Metric', 'pointToPoint', 'MaxIterations', maxIter, 'Tolerance', [0.001, 0.001]);
             elapsedTime = toc;
             % tic + toc return elapsed time in seconds
             elapsedTime = elapsedTime * 1000;
